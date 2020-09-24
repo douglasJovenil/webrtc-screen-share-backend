95d5f3542b26e6754eba6105cceef92efed96830
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-09-24 07:32:29 -0300

    impl: logica onde nao remove o streamer do array de sockets

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# feracode_backend" 
+
+TODO: fazer um Map para os viewers e outro para os integrantes
--- src/server.ts
+++ src/server.ts
@@ -13,140 +13,136 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
-var viewers = new Map<string, socket.Socket>();
+var sockets = new Map<string, socket.Socket>();
 var streamer: socket.Socket;
 const maxUsersAtRoom = 3;
 
 app.use(cors());
 
 io.on('connection', (socket) => {
+  console.log('connection');
   // Quando alguem entra na sala
   socket.on('join_room', () => {
+    console.log('on: join_room');
     // Se a sala estiver cheia, informa o usuario e sai do metodo
-    if (getAllSockets().length > maxUsersAtRoom) {
+    if (sockets.size > maxUsersAtRoom) {
+      console.log('emit: full_room');
       socket.emit('full_room');
       return;
     }
 
-    // Salva o novo viewer na sala
-    viewers.set(socket.id, socket);
+    // Salva o novo integrante na sala
+    sockets.set(socket.id, socket);
 
     // Se a stream ja estiver acontecendo, informa o streamer
     // para criar um peer para o novo viewer
     if (streamer) {
+      console.log('emit: add_new_peer');
       streamer.emit('add_new_peer', socket.id);
-      // Informa que acabou de entrar no quem eh o streamer para atualizar a UI
+      // Informa quem acabou de entrar quem eh o streamer
+      console.log('emit: streamer_joined');
       socket.emit('streamer_joined', streamer.id);
     }
 
-    // Informa para quem acabou de entrar sobre os integrantes da sala para mostrar na UI
+    // Informa para quem acabou de entrar sobre os integrantes da sala
+    console.log('emit: send_viewers_of_room');
+    console.log(getAllSockets().map((currentSocket) => currentSocket.id));
     socket.emit(
       'send_viewers_of_room',
-      getAllIDs().filter((id) => id !== socket.id)
+      // getAllSockets().filter((currentSocket) => currentSocket.id !== socket.id)
+      getAllSocketsIDs().filter((id) => id !== socket.id)
     );
 
-    // Informa os integrantes da sala sobre quem acabou de entrar para mostrar na UI
-    getAllSockets().forEach((sock) => {
+    // Informa os integrantes da sala sobre quem acabou de entrar
+    sockets.forEach((sock) => {
       if (sock.id !== socket.id) {
+        console.log('emit: viewer_joined');
         sock.emit('viewer_joined', socket.id);
       }
     });
-
-    // Informa para o usuario seu id para mostrar na UI
-    socket.emit('join_accept', socket.id);
   });
 
-  // Quando algum viewer decide compartilhar a tela
+  // Quando algum integrante decide compartilhar a tela
   socket.on('start_stream', () => {
-    // Salva esse viewer como streamer
+    console.log('on: start_stream');
+    // Salva esse integrante como streamer
     streamer = socket;
 
-    // Remove o streamer do Map de viewers
-    viewers.delete(socket.id);
-
     // Devolve os viewers para o streamer
-    socket.emit('create_peers_to_start_stream', Array.from(viewers.keys()));
-    // Informa os viewers quem comecou a stream
-    getAllSockets().forEach(sock => sock.emit('streamer_joined', socket.id));
+    console.log('emit: create_viewer_peers');
+    socket.emit(
+      'create_viewers_peers',
+      getAllViewers().map((viewer) => viewer.id)
+    );
+
+    console.log('emit: streamer_joined');
+    // Informa os integrantes quem eh o novo streamer
+    sockets.forEach((currentSocket) =>
+      currentSocket.emit('streamer_joined', socket.id)
+    );
   });
 
   // Quando o streamer parar de compartilhar a tela;
   socket.on('stop_stream', () => {
+    console.log('on: stop_stream');
     // Para indicar que novas streams sao possiveis
     streamer = null;
 
-    // Adiciona os streamer que parou a transmissao como viewer
-    viewers.set(socket.id, socket);
+    console.log('emit: streamer_joined');
     // Informa os viewers que o streamer parou a stream
-    getAllSockets().forEach(sock => sock.emit('streamer_joined', ''));
+    getAllViewers().forEach((sock) => sock.emit('streamer_joined', ''));
   });
 
   // Quando o streamer notifica os viewers que vai iniciar a transmissao
   // Um peer por vez é enviado para cada viewer
   socket.on('send_offer', (payload: OfferPayload) => {
-    viewers.forEach((viewer) => {
-      // Garante que o viewer que deve criar uma answer
-      // eh o da iteracao atual do streamer
-      if (viewer.id === payload.socketID) {
-        // solicita que o viewer crie uma resposta para o peer
-        viewer.emit('create_answer', payload.signal);
-      }
-    });
+    sockets.get(payload.socketID).emit('create_answer', payload.signal);
   });
 
   // Quando um viewer cria um peer e responde o streamer
   socket.on('send_answer', (signal: SignalData) => {
+    console.log('on: send_answer');
     // Passa a resposta do viewer para o streamer para finalizar o handshake
+    console.log('emit: accept_answer');
     streamer.emit('accept_answer', { signal: signal, socketID: socket.id });
   });
 
   // Quando alguem sai da sala
   socket.on('disconnect', () => {
+    console.log('on: disconnect');
+
+    console.log('emit: delete_peer');
     // Informa o streamer para deletar o viewer que saiu da sala
-    if (streamer) {
-      streamer.emit('delete_peer', socket.id);
+    if (streamer) streamer.emit('delete_peer', socket.id);
 
-      // Se o streamer sair, novas streams estao disponiveis
-      if (streamer.id === socket.id) streamer = null;
-    }
+    // Se o streamer sair, novas streams estao disponiveis
+    if (streamer && streamer.id === socket.id) streamer = null;
 
+    console.log('emit: viewer_quit');
     // Informa todos que o usuário saiu
-    emitToAll('viewer_quit', socket.id);
+    sockets.forEach((currentSocket) =>
+      currentSocket.emit('viewer_quit', socket.id)
+    );
 
     // Remove o socket do viewer que saiu da sala
-    viewers.delete(socket.id);
+    sockets.delete(socket.id);
   });
 });
 
-server.listen(process.env.PORT || 8080, () => console.log('Rodando servidor na porta 8080'));
-
-function emitToAll(topic: string, value: any) {
-  if (streamer) {
-    streamer.emit(topic, value);
-  }
-
-  viewers.forEach((viewer) => viewer.emit(topic, value));
-}
-
-function getAllIDs(): string[] {
-  const ret: string[] = [];
-
-  if (streamer) {
-    ret.push(streamer.id);
-  }
-
-  viewers.forEach((viewer) => ret.push(viewer.id));
-  return ret;
-}
+server.listen(process.env.PORT || 8080, () =>
+  console.log('Rodando servidor na porta 8080')
+);
 
 function getAllSockets(): SocketIO.Socket[] {
-  const ret: SocketIO.Socket[] = [];
-  
-  if (streamer) {
-    ret.push(streamer);
-  }
+  return Array.from(sockets.values());
+}
 
-  viewers.forEach((viewer) => ret.push(viewer));
+function getAllViewers(): SocketIO.Socket[] {
+  return Array.from(sockets.values()).filter(
+    (currentSocket) => currentSocket !== streamer
+  );
+}
 
-  return ret;
+function getAllSocketsIDs(): string[] {
+  return Array.from(sockets.keys());
 }
